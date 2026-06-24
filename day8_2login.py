2108211e60c6515d24ebf0704d9734fceb199e89
이동수 <leedong4678@gmail.com>
2026-06-24 14:01:43 +0900

    CORS 설정 수정: allow_origins 와일드카드 + credentials 비활성화

--- day8_2login.py
+++ day8_2login.py
@@ -5,8 +5,8 @@ from pydantic import BaseModel
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React 개발 서버만 허용(허용할 출처)
-    allow_credentials=True,  #(인증정보 허용, 쿠키, jWT토큰 등등)
+    allow_origins=["*"],     # 실제 배포시에는 허용할 프론트엔드 도메인만 지정하는 것이 좋습니다.
+    allow_credentials=False, #(인증정보 허용, 쿠키, jWT토큰 등등)
     allow_methods=["*"],     #허용할 HTTP메서드(get, post, patch, put, delete)
     allow_headers=["*"],     #허용할 http헤더(Content-Type, Authorization)
 )
