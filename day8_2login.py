4841f9aa70eb849fb9909143353c6d3d7429f71d
이동수 <leedong4678@gmail.com>
2026-06-24 13:10:26 +0900

    day8_2login.py 로그인 검증 로직 및 CORS 설정 추가

--- day8_2login.py
+++ day8_2login.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # React 개발 서버만 허용(허용할 출처)
+    allow_credentials=True,  #(인증정보 허용, 쿠키, jWT토큰 등등)
+    allow_methods=["*"],     #허용할 HTTP메서드(get, post, patch, put, delete)
+    allow_headers=["*"],     #허용할 http헤더(Content-Type, Authorization)
+)
 
 class Login(BaseModel):
     userid: str
